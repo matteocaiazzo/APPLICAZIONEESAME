aeeba66b0e272b6ca87f049fc86c122613f28b8e
matteocaiazzo <matteo.caiazzo@libero.it>
2019-06-06 16:44:18 +0200

    aggiunta classe per il download del json

---
+++ app/src/main/java/com/androstock/myweatherapp/GoodModel.java
@@ -0,0 +1,57 @@
+package com.androstock.myweatherapp;
+
+public class GoodModel {
+
+    private String Luogo, Orari, Telefono, Servizi, SitoWeb;
+
+
+
+    public String getLuogo(){
+        return Luogo;
+    }
+
+    public void setLuogo(String Luogo){
+        this.Luogo = Luogo;
+    }
+
+
+
+    public String getOrari() {
+        return Orari;
+    }
+
+    public void setOrari(String Orari) {
+        this.Orari = Orari;
+    }
+
+
+
+    public String getTelefono() {
+        return Telefono;
+    }
+
+    public void setTelefono(String Telefono) {
+        this.Telefono = Telefono;
+    }
+
+
+
+    public String getServizi() {
+        return Servizi;
+    }
+
+    public void setServizi(String Servizi) {
+        this.Servizi = Servizi;
+    }
+
+
+    public String getSitoWeb() {
+        return SitoWeb;
+    }
+
+    public void setSitoWeb(String SitoWeb) {
+        this.SitoWeb = SitoWeb;
+    }
+
+
+}
